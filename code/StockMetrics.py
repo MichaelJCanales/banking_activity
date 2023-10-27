1b5d939990b78fb6c69c297526f25170e35d9a4b
Michael Canales <mikeycanales10@gmail.com>
2023-10-27 18:49:26 -0400

    median

--- code/StockMetrics.py
+++ code/StockMetrics.py
@@ -15,37 +15,45 @@ class StockMetrics(StockData):
         """pt1
         """
         averages = []
-        for row in self.data: 
+        # skip the dates
+        for row in self.data[1:]: 
             valid_prices = [] 
             
-            for stock_price in row:
+            for stock_price in row[1:]:
                 # skip any values that are empty strings then convert
                 if stock_price != "":
-                    # convert the strings to numbers
-                    price = float(stock_price) 
+                    # convert the strings to numbers and .append to add into the average
+                    price = float(stock_price)
+                    valid_prices.append(price) 
             
             # get the average and round it to nearest 3rd decimal
             if valid_prices:
                 average = stats.mean(valid_prices)
                 rounded_average = round(average, 3)
                 
-            # append round_average to averages
-            averages.append(rounded_average)
+                # append round_average to averages
+                averages.append(rounded_average)
             
         return averages
         
     def median02(self):
         """pt2
         """
-        median = []
+        # repurpose code from averages but don't round 
+        medians = []
         
         for row in self.data:
             valid_prices =[]
             for stock_price in row:
                 if stock_price != "":
                     price = float(stock_price)
+                    valid_prices.append(price)
+                    
+            if valid_prices:
+                median_row = stats.median(valid_prices)
+                medians.append(median_row)
             
-                
+        return medians
                     
         ...
 
