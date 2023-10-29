ccf2147cc00d188bd112675e929cd516887f084d
Michael Canales <mikeycanales10@gmail.com>
2023-10-29 01:16:17 -0400

    passed PT1&2

--- code/StockMetrics.py
+++ code/StockMetrics.py
@@ -1,42 +1,34 @@
 import statistics as stats
 from code.StockData import StockData
-import csv
+
 
 
 class StockMetrics(StockData):
-    def __init__(self, path, filename):
+    def __init__(self, path):
         # call the parent method's constructor
         super(StockMetrics, self).__init__(path)
 
         # now that we've ran self.load(), we can interact with "self.data" as a
         # list of lists
         self.load()
-        self.filename = filename
-        self.data = []
         
-    def read_csv(self):
-        with open(self.filename, 'r') as file: 
-            reader = csv.reader(file)
-            
-            next(reader)
-            
-            for row in reader:
-                self.data.append(row)
-
     def average01(self):
         """pt1
         """
         averages = []
         # skip the dates
-        for row in self.data[1:]: 
+        for row in self.data: 
             valid_prices = [] 
             
             for stock_price in row[1:]:
-                # skip any values that are empty strings then convert
+                # skip any empty strings then convert
                 if stock_price != "":
-                    # convert the strings to numbers and .append to add into the average
-                    price = float(stock_price)
-                    valid_prices.append(price) 
+                    try:
+                    # convert the strings to numbers and .append into the average
+                        price = float(stock_price)
+                        valid_prices.append(price) 
+                    except ValueError:
+                        continue
             
             # get the average and round it to nearest 3rd decimal
             if valid_prices:
@@ -58,9 +50,11 @@ class StockMetrics(StockData):
             valid_prices =[]
             for stock_price in row:
                 if stock_price != "":
-                    price = float(stock_price)
-                    valid_prices.append(price)
-                    
+                    try:
+                        price = float(stock_price)
+                        valid_prices.append(price)
+                    except ValueError:
+                        continue
             if valid_prices:
                 median_row = stats.median(valid_prices)
                 medians.append(median_row)
@@ -70,4 +64,4 @@ class StockMetrics(StockData):
     def stddev03(self):
         """pt3
         """
-        ...
+        
